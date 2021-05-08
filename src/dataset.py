f1e412c917358aeefa8ec46a1fc3b9d0bdef1a40
Tomas Willaschek <willaschek.t@gmail.com>
2021-05-08 14:20:55 +0200

    added noise reduction to speech loader

--- src/dataset.py
+++ src/dataset.py
@@ -69,9 +69,46 @@ class DatasetLoader:
     def load_sound(self, path):
         """Load sound to MFSS format"""
         wave, sr = librosa.load(path, mono=True, sr=None)
+        # Trim beginning and ending silence
+        wave2, index = librosa.effects.trim(wave)
         # Downsampling
-        wave = wave[::3]
-        mfcc = librosa.feature.mfcc(wave, sr, hop_length=int(sr/100), n_fft=int(sr/40))
+        wave2 = wave2[::3]
+        mfcc = librosa.feature.mfcc(wave2, sr, hop_length=int(sr/100), n_fft=int(sr/40))
+
+        # import soundfile as sf
+        # p = '/home/tezzo/Desktop/sur/train/23/m424_02_r07_i0_0.wav'
+        # y, sr = librosa.load(p, mono=True, sr=None)
+        # # sf.write('/home/tezzo/Desktop/test.wav', samplerate=sr, data=)
+        #
+        # S_full, phase = librosa.magphase(librosa.stft(y))
+        # S_filter = librosa.decompose.nn_filter(S_full,
+        #                                        aggregate=np.median,
+        #                                        metric='cosine',
+        #                                        width=int(librosa.time_to_frames(2, sr=sr)))
+        # S_filter = np.minimum(S_full, S_filter)
+        # margin_i, margin_v = 2, 10
+        # power = 2
+        # mask_i = librosa.util.softmask(S_filter,
+        #                                margin_i * (S_full - S_filter),
+        #                                power=power)
+        # mask_v = librosa.util.softmask(S_full - S_filter,
+        #                                margin_v * S_filter,
+        #                                power=power)
+        # # Once we have the masks, simply multiply them with the input spectrum
+        # # to separate the components
+        # S_foreground = mask_v * S_full
+        # S_background = mask_i * S_full
+        #
+        # # apply mask
+        # yf = librosa.istft(S_foreground)
+        # sf.write('/home/tezzo/Desktop/test.wav', samplerate=sr, data=yf)
+        #
+        # # volume boost
+        # y_aug = yf.copy()
+        # dyn_change = np.random.uniform(low=1.5, high=3)
+        # y_aug = y_aug * dyn_change
+        # sf.write('/home/tezzo/Desktop/test2.wav', samplerate=sr, data=y_aug)
+
         return mfcc
 
     def check_image_dataset(self): # Check if dataset is correct
@@ -91,5 +128,3 @@ class DatasetLoader:
     @property
     def train_count(self):
         return self.x_train.shape[0]
-
-
