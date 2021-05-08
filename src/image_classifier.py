3cfbc0bf0ebd31aadfb409aa5def157a9503a14a
TomasVenkrbec <xvenkr01@stud.fit.vutbr.cz>
2021-05-08 02:40:23 +0200

    fixed image data generator, 67.7% accuracy

--- src/image_classifier.py
+++ src/image_classifier.py
@@ -40,7 +40,7 @@ class ImageClassifier():
         cnn = self.add_conv_block(inp, 6, 0.2) # 40x40
         cnn = self.add_conv_block(cnn, 6, 0.35) # 20x20
         cnn = self.add_conv_block(cnn, 6, 0.5) # 10x10
-        #cnn = self.add_conv_block(cnn, 8) # 5x5
+        #cnn = self.add_conv_block(cnn, 6) # 5x5
 
         cnn = Flatten()(cnn)
         out = Dense(self.dataset.class_count, activation="softmax", kernel_initializer="he_uniform")(cnn)
@@ -54,18 +54,18 @@ class ImageClassifier():
         # Create callbacks
         tensorboard_callback = TensorBoard(histogram_freq=1, write_images=True)
         modelcheckpoint_callback = ModelCheckpoint(snapshot_path, monitor="val_categorical_accuracy", save_best_only=True, save_weights_only=True)
-        earlystopping_callback = EarlyStopping(patience=30, monitor="val_categorical_accuracy", baseline=0.3)
+        earlystopping_callback = EarlyStopping(patience=30, monitor="val_categorical_accuracy", baseline=0.1)
 
         # Change <1;31> labels to <0;30>
         y_val = np.subtract(self.dataset.y_val, self.dataset.class_shift)
         y_train = np.subtract(self.dataset.y_train, self.dataset.class_shift)
 
         # Data augmentation
-        datagen = ImageDataGenerator(zoom_range=[0.9,1.1], brightness_range=[0.90,1.10])
+        datagen = ImageDataGenerator(preprocessing_function=(lambda img: (img - 127.5) / 127.5), width_shift_range=0.05, height_shift_range=0.05, zoom_range=[0.95,1.05], brightness_range=[0.90,1.1])
         train_gen = datagen.flow(self.dataset.x_train, to_categorical(y_train), batch_size=self.batch_size)
-        
+
         # Train model
-        self.classifier.fit(self.dataset.x_train, to_categorical(y_train), batch_size=self.batch_size, epochs=self.epochs, callbacks=[earlystopping_callback, modelcheckpoint_callback], validation_data=(self.dataset.x_val, to_categorical(y_val)))
+        self.classifier.fit(train_gen, batch_size=self.batch_size, epochs=self.epochs, callbacks=[earlystopping_callback, modelcheckpoint_callback], validation_data=(self.dataset.x_val, to_categorical(y_val)))
         
         # Load best weights and evaluate accuracy
         self.classifier.load_weights(snapshot_path)
